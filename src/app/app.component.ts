f0c4da5274c13b197a617125be3984c2e090a819
thaua-engelmann <engelmann.webdev@gmail.com>
2024-08-10 14:07:06 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
+import { HomeComponent } from '@ecommerce/home';
+
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, HomeComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
